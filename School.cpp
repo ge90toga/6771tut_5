d9a200f26fb9a4dc0b705d2549550ed652466262
ge90toga <liquan1992@outlook.com>
2016-09-02 18:17:27 -0700

    finished version v1.0

--- School.cpp
+++ School.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include "School.h"
 
 // init the student number counter
@@ -7,8 +8,8 @@ unsigned int School::Student::nextStudentNumber = 9312u;
 // return false if the student is already enrolled in the course
 bool School::Course::addStudent(std::weak_ptr<Student> wp) {
     // check that the student isn't already in the course
-    auto result = std::find_if(studentsInCourse.begin(),studentsInCourse.end(), [wp] (std::weak_ptr<Student> s) {
-        if ( wp.lock() == s.lock() ) return true;
+    auto result = std::find_if(studentsInCourse.begin(), studentsInCourse.end(), [wp](std::weak_ptr<Student> s) {
+        if (wp.lock() == s.lock()) return true;
         return false;
     });
     if (result != studentsInCourse.end()) return false;
@@ -21,8 +22,12 @@ bool School::Course::addStudent(std::weak_ptr<Student> wp) {
 void School::Course::printRoll() const {
     std::cout << courseCode << " Roll" << std::endl;
     for (auto wp : studentsInCourse) {
-        // TODO: print the names of the students in the course
-        // std::cout << sp->getName() << std::endl;
+        auto sp = wp.lock();
+        /*weak pointers must be converted to std::shared_ptr
+        in order to access the referenced object.*/
+        if (sp != nullptr) {
+            std::cout << sp->getName() << std::endl;
+        }
     }
 }
 
@@ -38,8 +43,8 @@ bool School::Student::addCourse(std::weak_ptr<Course> wp) {
 void School::Student::printTimetable() const {
     std::cout << "Timetable for " << name << " " << studentNumber << std::endl;
     for (auto c : coursesEnrolledIn) {
-        auto sp = c.lock();	// convert the weak_ptr to a shared_ptr
-        if (sp != nullptr) {	// if the convert was successful then print the course
+        auto sp = c.lock();    // convert the weak_ptr to a shared_ptr
+        if (sp != nullptr) {    // if the convert was successful then print the course
             std::cout << sp->getCourseCode() << std::endl;
         }
     }
@@ -47,22 +52,35 @@ void School::Student::printTimetable() const {
 
 // create a course object and add it to the list of courses
 // return false if the course already exists
-bool School::addCourse(const std::string& courseCode) {
+bool School::addCourse(const std::string &courseCode) {
     // TODO: double check that the course doesn't already exist.
     // create a new course object
+    auto result = std::find_if(courses.begin(), courses.end(),
+                               [&courseCode](std::shared_ptr<Course> c) {
+        if (c->getCourseCode() == courseCode) return true;
+        return false;
+    });
+
+    if (result != courses.end()) return false; //course already exists.
     courses.push_back(std::make_shared<Course>(courseCode));
     return true;
 }
 
 // method to add a student to a course.
-void School::addStudentToCourse(unsigned int id, const std::string& courseCode) {
-    auto courseObject = findCourse(courseCode);	// find the course object
+void School::addStudentToCourse(unsigned int id, const std::string &courseCode) {
+    auto courseObject = findCourse(courseCode);    // find the course object
     // find student
     auto studentObject = findStudent(id);
 
     //TODO: try to add the student to the course.
     // if the student can't be added to the course, e.g. they are already enrolled
     // then return false.
+    // courseObject is an iterator of std::vector<std::shared_ptr<Course>>
+    if (!(*courseObject)->addStudent(studentObject)) {
+        std::stringstream ss;
+        ss << "Student " << id << " is already enrolled in course " << courseCode;
+        throw std::runtime_error{ss.str()};
+    }
 
     // add the course object to a student's object
     studentObject->addCourse(*courseObject);
@@ -72,8 +90,9 @@ void School::addStudentToCourse(unsigned int id, const std::string& courseCode) 
 // there are enough lockers available.
 bool School::assignLocker(unsigned int id) {
     auto studentObject = findStudent(id);
-    if ( lockers.size() > 0u) {
+    if (lockers.size() > 0u) {
         // TODO: assign the locker to the student
+        studentObject->assignLocker(std::move(lockers.back()));
         lockers.pop_back();
         return true;
     }
@@ -83,9 +102,9 @@ bool School::assignLocker(unsigned int id) {
 // method to find a course object.
 // showcasing a trailing return type, using the type of iterator that courses returns.
 // this is used as trying to determine the type of this iterator is difficult.
-auto School::findCourse(const std::string& courseCode) const -> decltype(courses.begin()) {
+auto School::findCourse(const std::string &courseCode) const -> decltype(courses.begin()) {
     // find course.
-    auto courseObject = std::find_if(courses.begin(),courses.end(),[&courseCode] (std::shared_ptr<Course> c) {
+    auto courseObject = std::find_if(courses.begin(), courses.end(), [&courseCode](std::shared_ptr<Course> c) {
         if (c->getCourseCode().compare(courseCode) == 0) return true;
         return false;
     });
